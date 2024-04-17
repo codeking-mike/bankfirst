38bc21ae89988eda6590b8b40dd165b1066bc512
Michael Idam <princemicare@gmail.com>
2024-04-17 12:06:32 +0100

    finished the front end of the bank project

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,4 +5,24 @@ btn.addEventListener("click", () =>{
     btn.classList.toggle('open');
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
-})
+});
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.carousel-item');
+  slides.forEach(slide => {
+    slide.style.opacity = 0;
+  });
+  slides[index].style.opacity = 1;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % 3; // 3 is the number of slides
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + 3) % 3; // 3 is the number of slides
+  showSlide(currentSlide);
+}
